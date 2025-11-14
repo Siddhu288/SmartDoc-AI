fd310af1ce18857c09c2f4da219ae4d941a551be
Shiva-0515 <shivakanchanapelli24@gmail.com>
2025-11-14 20:51:14 +0530

    backend url updated

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,11 +1,18 @@
+// import axios from "axios";
+
+// const isLocalhost = window.location.hostname === "localhost";
+
+// const api = axios.create({
+//   baseURL: isLocalhost
+//     ? "http://localhost:8000" // local FastAPI backend
+//     : "https://smartdoc-ai-t8p8.onrender.com", // Render backend URL
+// });
+
+// export default api;
 import axios from "axios";
 
-const isLocalhost = window.location.hostname === "localhost";
-
 const api = axios.create({
-  baseURL: isLocalhost
-    ? "http://localhost:8000" // local FastAPI backend
-    : "https://smartdoc-ai-t8p8.onrender.com", // Render backend URL
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
 });
 
 export default api;
